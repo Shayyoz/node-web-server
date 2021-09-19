2cd7d23de85a3326a726880bda58310026174498
Shayyoz <shayizak88@gmail.com>
2021-09-19 17:30:11 +0300

    aa

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 const hbs = require('hbs')
 const { EEXIST } = require('constants')
-
+ 
 const app = express()
 const port = process.env.PORT || 3000
 const publicDirectory = path.join(__dirname,'../public')
